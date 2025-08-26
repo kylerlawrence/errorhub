da02f15b97a086061820b6246f157a84a879fe21
Kyle Lawrence <19819383+kylerlawrence@users.noreply.github.com>
2025-08-26 15:48:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/errorhub/', // use your repo name here
   plugins: [react()],
-})
+  base: "/errorhub/", // 👈 VERY IMPORTANT
+});
